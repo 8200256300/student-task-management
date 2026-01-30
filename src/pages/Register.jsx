a558b9aa8f7c74052a3b90cc7cad748d970294ef
Mahil <khanparamahil2710.com>
2026-01-30 13:48:33 +0530

    [MK] [Register page]

---
+++ src/pages/Register.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import '../index.css'
+
+const Register = () => {
+  return (
+    <>
+      <div classNmae="form-container">
+        <h1>Register</h1>
+        </div>
+    </>
+  )
+}
+
+export default Register
