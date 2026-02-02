eb3aebf2649f86e7bc31908ad2b42c60bdbe684b
Mahil <khanparamahil2710.com>
2026-02-02 13:35:17 +0530

    file updated

--- src/App.jsx
+++ src/App.jsx
@@ -1,21 +1,33 @@
-import "./App.css";
-import Login from "./pages/Login";
-import Register from "./pages/Register";
-import { createBrowserRouter, RouterProvider } from "react-router-dom";
+import './App.css'
+import Login from './pages/Login'
+import Register from './pages/Register'
+import { createBrowserRouter, Navigate, RouterProvider } from 'react-router-dom'
 
-const router = createBrowserRouter([
-  {
-    path: "/Login",
-    element: <Login />,
-  },
-  {
-    path: "/register",
-    element: <Register />,
-  },
-]);
-
+const DefaultRoute =() => {
+  const authData = JSON.parse(localStorage.getItem('authData'));
+  if(authData){
+      return<Navigate to = "/Login" replace/>;
+  }
+  return <Navigate to = "/Register" replace/>;
+}
+  
 function App() {
-  return <RouterProvider router={router} />;
+const route = createBrowserRouter([
+  {
+    path:"/",
+    element: <DefaultRoute/>
+  },
+    {
+      path: "/Login",
+      element: <Login />
+    },
+    {
+      path: "/Register",
+      element: <Register />
+    }
+  ])
+
+  return <RouterProvider router={route}/>
 }
 
-export default App;
+export default App
