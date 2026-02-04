b3f2ff8d6ce3ede2cada1c18bd4fffe0f6b9b5b8
Mahil <khanparamahil2710.com>
2026-02-04 13:37:35 +0530

    d

--- src/pages/Dashboard.jsx
+++ src/pages/Dashboard.jsx
@@ -1,9 +1,38 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
+import Navbar from '../components/Navbar'
+import { Navigate, useNavigate } from 'react-router-dom';
+import TaskList from '../components/TaskList';
 
 const Dashboard = () => {
+
+  const Navigate = useNavigate()
+  const [tasks, setTasks] = useState([])
+
+  useEffect(() =>{
+    fetchData()
+  })
+
+  const fetchData = async() => {
+    try {
+      const response = await fetch("http://localhost:3000/tasks");
+      const data = response.json();
+      setTasks(data);
+  }catch (error) {
+    console.log(error)
+  }
+}
+  const handleLogout = () => {
+    localStorage.removeItem("loginData");
+    // localStorage.removeItem("authData");
+    // localStorage.clear();
+    Navigate('/Login')
+  }
+
   return (
     <div>
-      <h1>Welcome To Dashboard</h1>
+      <Navbar onLogout={handleLogout}/>
+      <h1>MY TASKS</h1>
+      <TaskList/>
     </div>
   )
 }
